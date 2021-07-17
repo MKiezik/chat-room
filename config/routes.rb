80dcaae46f83968b98d197040ab015e437d4e398
mkiezik <m.kiezik@intratel.pl>
2021-07-17 13:31:21 +0200

    Room CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'messages/index'
-  get 'rooms/index'
+  resources :rooms
+
+  root 'rooms#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
